31eb0a5313e0485a4543dd0c3f5e20f3084dfce2
MauroPello <mauro.pellonara@outlook.com>
2025-09-28 22:54:17 +0200

    finish exercises

--- labs/ex03/template/build_polynomial.py
+++ labs/ex03/template/build_polynomial.py
@@ -6,10 +6,19 @@ import numpy as np
 
 def build_poly(x, degree):
     """polynomial basis functions for input data x, for j=0 up to j=degree."""
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # polynomial basis function: TODO
-    # this function should return the matrix formed
-    # by applying the polynomial basis to the input data
-    # ***************************************************
-    raise NotImplementedError
+    # N = x.shape[0]
+    # d = degree + 1
+    # poly = np.zeros(shape=(N, d))
+
+    # for i in range(N):
+    #     for j in range(d):
+    #         poly[i, j] = x[i] ** j
+
+    # return poly
+
+    # This is a vectorized version of the nested for loops.
+    # It uses numpy's broadcasting to compute the powers.
+    # x[:, np.newaxis] has shape (N, 1)
+    # np.arange(degree + 1) has shape (degree+1,)
+    # The result of the power operation is a (N, degree+1) array.
+    return x[:, np.newaxis] ** np.arange(degree + 1)
